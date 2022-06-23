4444c6bff89177e3096d36399d600fd137289ba9
Ignacio07 <65515081+Ignacio07@users.noreply.github.com>
2022-06-23 18:49:38 -0400

    Update Detector.cpp

--- src/Detector.cpp
+++ src/Detector.cpp
@@ -17,16 +17,14 @@ void Detector::toggleMode() { m = (m == Default ? Daimler : Default); }
 string Detector::modeName() const { return (m == Default ? "Default" : "Daimler"); }
 
 vector<Persona> Detector::detect(InputArray img) {
-    // Run the detector with default parameters. to get a higher hit-rate
-    // (and more false alarms, respectively), decrease the hitThreshold and
-    // groupThreshold (set groupThreshold to 0 to turn off the grouping completely).
+    // Se ejectuta el Detector con los parametros pre establecidos, para obtener resultados mas existosos.
     vector<Rect> found;
     if (m == Default)
         hog.detectMultiScale(img, found, 0, Size(2, 2), Size(4, 4), 1.05, 2, false);
     else if (m == Daimler)
         hog_d.detectMultiScale(img, found, 1, Size(2, 2), Size(4, 4), 1.05, 3, true);
 
-    // Convertir un objeto Rect a un objeto persona
+    // Convertir un objeto Rect a un objeto persona.
     vector<Persona> personas;
 
     for (vector<Rect>::iterator i = found.begin(); i != found.end(); ++i) {
@@ -39,8 +37,8 @@ vector<Persona> Detector::detect(InputArray img) {
 }
 
 void Detector::adjustRect(Rect& r) const {
-    // The HOG detector returns slightly larger rectangles than the real objects,
-    // so we slightly shrink the rectangles to get a nicer output.
+    // El HOG detector retorna un rectangulo un poco mas grande que el objeto real,
+    // asi que ajustamos el rectangulo para que se vea mejor.
     r.x += cvRound(r.width * 0.1);
     r.width = cvRound(r.width * 0.8);
     r.y += cvRound(r.height * 0.07);
